52733022c52ecfdf1ffdae2afc0104b38c23aa1c
CodeOfWarrior <chenhappylife@163.com>
2017-04-28 17:20:23 +0800

    Signed-off-by: CodeOfWarrior <chenhappylife@163.com>

--- mytestdev/src/com/echarging/util/GoogleOrderValidate.java
+++
@@ -1,9 +0,0 @@
-package com.echarging.util;
-
-public class GoogleOrderValidate {
-	
-	public static boolean validateOrder(String signtureData,String signture){
-		return RSASignature.doCheck(signtureData, signture);
-	}
-
-}
